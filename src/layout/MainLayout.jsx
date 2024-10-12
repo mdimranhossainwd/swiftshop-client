a615ecdb6e7f51f206ec02e49c7a1c297587b190
mdimranhossainwd <mdimranhossain7498@gmail.com>
2024-10-12 19:01:37 +0600

    Footer sections Added

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,9 +1,11 @@
+import Footer from "../components/Footer";
 import HeaderMenu from "../components/Navbar";
 
 const MainLayout = () => {
   return (
     <div>
       <HeaderMenu />
+      <Footer />
     </div>
   );
 };
