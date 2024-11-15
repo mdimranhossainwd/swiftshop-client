dc77e1f05f2ca3502029677449325337a40efb30
mdimranhossainwd <mdimranhossain7498@gmail.com>
2024-11-15 19:22:16 +0600

    func: my cart pages functionalitty added

--- src/pages/MyCartPages.jsx
+++ src/pages/MyCartPages.jsx
@@ -51,111 +51,124 @@ const MyCartPages = () => {
           || Your Cart Page ||
         </h3>
       </div>
-      <div className="flex flex-col lg:flex-row gap-8 p-4">
-        {/* Product List Section */}
-        <div className="flex-1 bg-white rounded-lg p-6">
-          <table className="table w-full">
-            <thead className="w-full border-b border-t text-black text-center">
-              <tr>
-                <th className="px-4 py-3">Product Name</th>
-                <th className="px-4 py-3">Quantity</th>
-                <th className="px-4 py-3">SubTotal</th>
-                <th className="px-4 py-3"></th>
-              </tr>
-            </thead>
-            <tbody className="w-full py-4 text-center">
-              {cartItems.map((item) => (
-                <tr className="py-4 border-b" key={item._id}>
-                  <td className="flex items-center gap-4 p-8">
-                    <img
-                      src={item.images}
-                      className="w-24 h-20 border"
-                      alt=""
-                    />
-                    <div>
-                      <h4 className="text-md bg-gradient-to-r from-blue-700 via-blue-500 to-blue-400 bg-clip-text text-transparent font-medium">
-                        {item.name}
-                      </h4>
-                      <span className="p-4 font-semibold">${item.price}</span>
-                    </div>
-                  </td>
-                  <td className="font-medium">
-                    <button
-                      className="px-2 py-1 mx-3 text-lg rounded-md"
-                      onClick={() => updateQuantity(item._id, -1)}
-                    >
-                      -
-                    </button>
-                    {item.quantity}
-                    <button
-                      className="px-2 py-1 mx-3 text-lg rounded-md"
-                      onClick={() => updateQuantity(item._id, 1)}
-                    >
-                      +
-                    </button>
-                  </td>
-                  <td className="font-medium">
-                    <span className="p-4 font-semibold">
-                      ${(item.price * item.quantity).toFixed(2)}
-                    </span>
-                  </td>
-                  <td>
-                    <button onClick={() => handleDelete(item._id)}>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="20"
-                        height="20"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        className="lucide mt-2 lucide-trash"
-                      >
-                        <path d="M3 6h18" />
-                        <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
-                        <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
-                      </svg>
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
 
-        {/* Cart Summary Section */}
-        <div className="flex-1 max-w-sm bg-white rounded-lg border p-6">
-          <div className="bg-blue-100 text-center py-4 rounded-md mb-6">
-            <p>
-              Congratulations! You've got{" "}
-              <span className="font-semibold text-blue-600">
-                Free Shipping!
-              </span>
-            </p>
+      {cart.length === 0 ? (
+        <div className="flex flex-col items-center ">
+          <img
+            src="https://static.vecteezy.com/system/resources/previews/025/343/104/non_2x/empty-folder-no-result-document-file-data-not-found-concept-empty-state-ui-infographic-free-vector.jpg"
+            className="w-1/2 md:w-1/3"
+          />
+          <p className="text-lg font-semibold mt-4 mb-14">
+            Your cart is empty!
+          </p>
+        </div>
+      ) : (
+        <div className="flex flex-col lg:flex-row gap-8 p-4">
+          {/* Product List Section */}
+          <div className="flex-1 bg-white rounded-lg p-6">
+            <table className="table w-full">
+              <thead className="w-full border-b border-t text-black text-center">
+                <tr>
+                  <th className="px-4 py-3">Product Name</th>
+                  <th className="px-4 py-3">Quantity</th>
+                  <th className="px-4 py-3">SubTotal</th>
+                  <th className="px-4 py-3"></th>
+                </tr>
+              </thead>
+              <tbody className="w-full py-4 text-center">
+                {cartItems.map((item) => (
+                  <tr className="py-4 border-b" key={item._id}>
+                    <td className="flex items-center gap-4 p-8">
+                      <img
+                        src={item.images}
+                        className="w-24 h-20 border"
+                        alt=""
+                      />
+                      <div>
+                        <h4 className="text-md bg-gradient-to-r from-blue-700 via-blue-500 to-blue-400 bg-clip-text text-transparent font-medium">
+                          {item.name}
+                        </h4>
+                        <span className="p-4 font-semibold">${item.price}</span>
+                      </div>
+                    </td>
+                    <td className="font-medium">
+                      <button
+                        className="px-2 py-1 mx-3 text-lg rounded-md"
+                        onClick={() => updateQuantity(item._id, -1)}
+                      >
+                        -
+                      </button>
+                      {item.quantity}
+                      <button
+                        className="px-2 py-1 mx-3 text-lg rounded-md"
+                        onClick={() => updateQuantity(item._id, 1)}
+                      >
+                        +
+                      </button>
+                    </td>
+                    <td className="font-medium">
+                      <span className="p-4 font-semibold">
+                        ${(item.price * item.quantity).toFixed(2)}
+                      </span>
+                    </td>
+                    <td>
+                      <button onClick={() => handleDelete(item._id)}>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="20"
+                          height="20"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          className="lucide mt-2 lucide-trash"
+                        >
+                          <path d="M3 6h18" />
+                          <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
+                          <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
+                        </svg>
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
           </div>
-          <h3 className="text-lg font-semibold mb-4">Cart Totals</h3>
-          <div className="space-y-4">
-            <div className="flex border-t border-b py-4 justify-between text-lg font-medium">
-              <p>Subtotal: ${totalCost.toFixed(2)}</p>
-            </div>
-            <div className="flex border-t border-b py-4 justify-between text-lg font-medium">
+
+          {/* Cart Summary Section */}
+          <div className="flex-1 lg:max-w-sm bg-white rounded-lg border p-6">
+            <div className="bg-blue-100 text-center py-4 rounded-md mb-6">
               <p>
-                Order Totals:{" "}
-                <span className="text-[#128AED] font-bold">
-                  ${totalCost.toFixed(2)}
+                Congratulations! You've got{" "}
+                <span className="font-semibold text-blue-600">
+                  Free Shipping!
                 </span>
               </p>
             </div>
-            <div>
-              <button className="w-full bg-blue-500 text-white py-3 font-oswald text-lg uppercase rounded-sm mt-4">
-                CheckOut
-              </button>
+            <h3 className="text-lg font-semibold mb-4">Cart Totals</h3>
+            <div className="space-y-4">
+              <div className="flex border-t border-b py-4 justify-between text-lg font-medium">
+                <p>Subtotal: ${totalCost.toFixed(2)}</p>
+              </div>
+              <div className="flex border-t border-b py-4 justify-between text-lg font-medium">
+                <p>
+                  Order Totals:{" "}
+                  <span className="text-[#128AED] font-bold">
+                    ${totalCost.toFixed(2)}
+                  </span>
+                </p>
+              </div>
+              <div>
+                <button className="w-full bg-blue-500 text-white py-3 font-oswald text-lg uppercase rounded-sm mt-4">
+                  CheckOut
+                </button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
