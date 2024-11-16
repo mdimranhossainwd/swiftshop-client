4297d84138b0791be29306593b8aa3334d59bf53
mdimranhossainwd <mdimranhossain7498@gmail.com>
2024-11-16 07:31:00 +0600

    ui: CustomerMenu sections Added

---
+++ src/components/menu/CustomerMenu.jsx
@@ -0,0 +1,58 @@
+import { NavLink } from "react-router-dom";
+
+const CustomerMenu = () => {
+  return (
+    <>
+      <NavLink
+        className={({ isActive }) => (isActive ? "text-[#128AED]" : "")}
+        to="profile"
+      >
+        Dashboard Overview
+      </NavLink>
+
+      <NavLink
+        className={({ isActive }) => (isActive ? "text-[#128AED]" : "")}
+        to="add-product"
+      >
+        My Orders
+      </NavLink>
+
+      <NavLink
+        className={({ isActive }) => (isActive ? "text-[#128AED]" : "")}
+        to="my-product"
+      >
+        Track Order
+      </NavLink>
+
+      <NavLink
+        className={({ isActive }) => (isActive ? "text-[#128AED]" : "")}
+        to="my-product"
+      >
+        Wishlist
+      </NavLink>
+
+      <NavLink
+        className={({ isActive }) => (isActive ? "text-[#128AED]" : "")}
+        to="review-queue"
+      >
+        Cart
+      </NavLink>
+
+      <NavLink
+        className={({ isActive }) => (isActive ? "text-[#128AED]" : "")}
+        to="payment"
+      >
+        Payment History
+      </NavLink>
+
+      <NavLink
+        className={({ isActive }) => (isActive ? "text-[#128AED]" : "")}
+        to="payment"
+      >
+        Account Settings
+      </NavLink>
+    </>
+  );
+};
+
+export default CustomerMenu;
