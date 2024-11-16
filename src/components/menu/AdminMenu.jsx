19189ecd3ffd8b858b16b1d82179c2d626efba68
mdimranhossainwd <mdimranhossain7498@gmail.com>
2024-11-16 07:38:43 +0600

    ui: Admin Menu Sections Added

---
+++ src/components/menu/AdminMenu.jsx
@@ -0,0 +1,88 @@
+import { NavLink } from "react-router-dom";
+
+const AdminMenu = () => {
+  return (
+    <>
+      <NavLink
+        className={({ isActive }) => (isActive ? "text-[#128AED]" : "")}
+        to="profile"
+      >
+        Admin Dashboard
+      </NavLink>
+
+      <NavLink
+        className={({ isActive }) => (isActive ? "text-[#128AED]" : "")}
+        to="add-product"
+      >
+        Manage User's
+      </NavLink>
+
+      <NavLink
+        className={({ isActive }) => (isActive ? "text-[#128AED]" : "")}
+        to="my-product"
+      >
+        Manage Products
+      </NavLink>
+      <NavLink
+        className={({ isActive }) => (isActive ? "text-[#128AED]" : "")}
+        to="my-product"
+      >
+        Add New Products
+      </NavLink>
+      <NavLink
+        className={({ isActive }) => (isActive ? "text-[#128AED]" : "")}
+        to="my-product"
+      >
+        Update Products
+      </NavLink>
+      <NavLink
+        className={({ isActive }) => (isActive ? "text-[#128AED]" : "")}
+        to="my-product"
+      >
+        All Orders
+      </NavLink>
+      <NavLink
+        className={({ isActive }) => (isActive ? "text-[#128AED]" : "")}
+        to="my-product"
+      >
+        Pending Orders
+      </NavLink>
+      <NavLink
+        className={({ isActive }) => (isActive ? "text-[#128AED]" : "")}
+        to="my-product"
+      >
+        Delivered Orders
+      </NavLink>
+
+      <NavLink
+        className={({ isActive }) => (isActive ? "text-[#128AED]" : "")}
+        to="my-product"
+      >
+        Revenue Analytics
+      </NavLink>
+
+      <NavLink
+        className={({ isActive }) => (isActive ? "text-[#128AED]" : "")}
+        to="review-queue"
+      >
+        Support Tickets
+      </NavLink>
+
+      <NavLink
+        className={({ isActive }) => (isActive ? "text-[#128AED]" : "")}
+        to="payment"
+      >
+        Promotions & Discounts
+      </NavLink>
+
+      <NavLink
+        className={({ isActive }) => (isActive ? "text-[#128AED]" : "")}
+        to="payment"
+      >
+        Support
+      </NavLink>
+    </>
+  );
+};
+
+export default AdminMenu;
