41d1b6cff8164f590e66e1a8cad06a5e9436e1b5
mdimranhossainwd <mdimranhossain7498@gmail.com>
2024-11-18 11:02:35 +0600

    func: checkoutform & payment method completed

--- src/components/form/CheckoutForm.jsx
+++ src/components/form/CheckoutForm.jsx
@@ -1,111 +1,117 @@
-import { CardElement } from "@stripe/react-stripe-js";
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
+import useAxios from "../../hooks/useAxios";
+import useCart from "../../hooks/useCart";
 import "./CheckoutForm.css";
 const CheckoutForm = () => {
-  //   const [course] = useCourse();
-  //   const totalCost = course.reduce((acc, cur) => acc + cur.price, 0);
-  //   const stripe = useStripe();
-  //   const elements = useElements();
-  //   const [clientSecret, setClientSecret] = useState("");
-  //   const { user } = useAuth();
-  //   const axios = useAxios();
-  //   const navigate = useNavigate();
-  //   const courseObject = course.reduce((acc, item, index) => {
-  //     acc[index] = item;
-  //     return acc;
-  //   }, {});
+  const [cart] = useCart();
+  const totalCost = cart.reduce((acc, cur) => acc + cur.price, 0);
+  const stripe = useStripe();
+  const elements = useElements();
+  const [clientSecret, setClientSecret] = useState("");
+  const { user } = useAuth();
+  const axios = useAxios();
+  const navigate = useNavigate();
+  const courseObject = cart.reduce((acc, item, index) => {
+    acc[index] = item;
+    return acc;
+  }, {});
 
-  //   useEffect(() => {
-  //     if (totalCost) {
-  //       getClientSecret(totalCost);
-  //     }
-  //   }, [totalCost]);
+  useEffect(() => {
+    if (totalCost) {
+      getClientSecret(totalCost);
+    }
+  }, [totalCost]);
 
-  //   // Get Client Secret
-  //   const getClientSecret = async (price) => {
-  //     try {
-  //       const { data } = await axios.post("/create-payment-intent", {
-  //         price: parseFloat(totalCost),
-  //       });
-  //       console.log(data);
-  //       setClientSecret(data.clientSecret);
-  //     } catch (err) {
-  //       console.log(err);
-  //     }
-  //   };
+  // Get Client Secret
+  const getClientSecret = async (price) => {
+    try {
+      const { data } = await axios.post("/create-payment-intent", {
+        price: parseFloat(totalCost),
+      });
+      console.log(data);
+      setClientSecret(data.clientSecret);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   //   // Post hangle Payment
-  //   const handlePayment = async (e) => {
-  //     e.preventDefault();
-  //     if (!stripe || !elements) {
-  //       return;
-  //     }
-  //     const card = elements.getElement(CardElement);
-  //     if (card === null) {
-  //       return;
-  //     }
-  //     const { paymentMethod, error } = await stripe.createPaymentMethod({
-  //       type: "card",
-  //       card,
-  //       billing_details: {
-  //         email: user?.email,
-  //         name: user?.displayName,
-  //       },
-  //     });
-  //     if (error) {
-  //       console.log("Payment Error", error);
-  //     } else {
-  //       console.log(paymentMethod);
-  //     }
+  const handlePayment = async (e) => {
+    const form = e.target;
+    const address = form.address.value;
+    e.preventDefault();
+    if (!stripe || !elements) {
+      return;
+    }
+    const card = elements.getElement(CardElement);
+    if (card === null) {
+      return;
+    }
+    const { paymentMethod, error } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+      billing_details: {
+        email: user?.email,
+        name: user?.displayName,
+      },
+    });
+    if (error) {
+      console.log("Payment Error", error);
+    } else {
+      console.log(paymentMethod);
+    }
 
-  //     const { error: confirmError, paymentIntent } =
-  //       await stripe.confirmCardPayment(clientSecret, {
-  //         payment_method: paymentMethod.id,
-  //       });
+    const { error: confirmError, paymentIntent } =
+      await stripe.confirmCardPayment(clientSecret, {
+        payment_method: paymentMethod.id,
+      });
 
-  //     if (confirmError) {
-  //       console.log("confirmErr", confirmError);
-  //     } else {
-  //       if (paymentIntent.status === "succeeded") {
-  //         const paymentInfo = {
-  //           email: user?.email,
-  //           name: user?.displayName,
-  //           status: "verified",
-  //           transId: paymentIntent.id,
-  //           courses: course.map((item) => ({
-  //             title: item?.title,
-  //             description: item?.description,
-  //             instructor: item?.instructor,
-  //             image_url: item?.image_url,
-  //             language: item?.language,
-  //             duration: item?.duration,
-  //           })),
-  //         };
-  //         try {
-  //           const { data } = await axios.post("/payment", paymentInfo);
-  //           console.log(data);
-  //           toast.success("Payment Successfully !");
-  //           navigate("/cart");
-  //         } catch (err) {
-  //           console.log(err);
-  //         }
-  //       }
-  //     }
-  //   };
+    if (confirmError) {
+      console.log("confirmErr", confirmError);
+    } else {
+      if (paymentIntent.status === "succeeded") {
+        const paymentInfo = {
+          address,
+          email: user?.email,
+          name: user?.displayName,
+          status: "completed",
+          transId: paymentIntent.id,
+          carts: cart.map((item) => ({
+            title: item?.title,
+            name: item?.name,
+            categoryId: item?.categoryId,
+            stock: item?.stock,
+            description: item?.description,
+            highlights: item?.highlights,
+            specifications: item?.specifications,
+            images: item?.images,
+            brand: item?.brand,
+            rating: item?.rating,
+            warranty: item?.warranty,
+            returnPolicy: item?.returnPolicy,
+            userName: item?.userName,
+            photoURL: item?.photoURL,
+            quantity: item?.quantity,
+          })),
+        };
+        try {
+          const { data } = await axios.post("/payment", paymentInfo);
+          console.log(data);
+          toast.success("Payment Successfully !");
+        } catch (err) {
+          console.log(err);
+        }
+      }
+    }
+  };
 
   return (
     <div className="grid grid-cols-1 space-y-10 mt-5 items-center justify-center">
-      <form>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-md font-semibold mb-1">
-            Your Full Name
-          </label>
-          <input
-            type="text"
-            id="name"
-            className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-            placeholder="Enter your name"
-          />
-        </div>
+      <form onSubmit={handlePayment}>
         <div className="mb-4">
           <label className="block text-gray-700 text-md font-semibold mb-1">
             Street Address
@@ -136,10 +142,10 @@ const CheckoutForm = () => {
         />
         <div className="flex items-center gap-4">
           <button
-            className="inline-flex w-full justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-[#7ed56f] focus-visible:ring-offset-2"
+            className="inline-flex w-full justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-bold "
             type="submit"
           >
-            Pay
+            Pay {totalCost}
           </button>
           <button className="inline-flex w-full justify-center rounded-md border border-transparent bg-red-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-[#7ed56f] focus-visible:ring-offset-2">
             Cancel
--- src/shared/Modal.jsx
+++ src/shared/Modal.jsx
@@ -42,7 +42,7 @@ const Modal = ({ isOpen, setIsOpen }) => {
                 <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
                   <Dialog.Title
                     as="h3"
-                    className="text-lg text-center gap-4 font-medium leading-6 text-gray-900"
+                    className="text-lg text-center flex items-center justify-center gap-4 font-medium leading-6 text-gray-900"
                   >
                     Secure Checkout
                     <svg
@@ -55,7 +55,7 @@ const Modal = ({ isOpen, setIsOpen }) => {
                       strokeWidth="2"
                       strokeLinecap="round"
                       strokeLinejoin="round"
-                      className="lucide lucide-hand-coins text-blue-600"
+                      className="lucide lucide-hand-coins text-center flex items-center text-blue-600"
                     >
                       <path d="M11 15h2a2 2 0 1 0 0-4h-3c-.6 0-1.1.2-1.4.6L3 17" />
                       <path d="m7 21 1.6-1.4c.3-.4.8-.6 1.4-.6h4c1.1 0 2.1-.4 2.8-1.2l4.6-4.4a2 2 0 0 0-2.75-2.91l-4.2 3.9" />
