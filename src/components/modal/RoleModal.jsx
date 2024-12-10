701e8f93a386cda071aff225bbcf13dbf5e239e0
mdimranhossainwd <mdimranhossain7498@gmail.com>
2024-12-10 13:12:09 +0600

    func: Admin delete a users functions

--- src/components/modal/RoleModal.jsx
+++ src/components/modal/RoleModal.jsx
@@ -4,6 +4,7 @@ import Modal from "../../shared/Modal";
 const RoleModal = ({ isOpen, setIsOpen, item, refetch }) => {
   const axios = useAxios();
 
+  // User's role changes functions
   const changeUserRole = async (id, currentRole) => {
     const newRole = currentRole === "customer" ? "admin" : "customer";
     try {
@@ -11,9 +12,18 @@ const RoleModal = ({ isOpen, setIsOpen, item, refetch }) => {
       toast.success(`User role updated to ${newRole}`);
       refetch();
     } catch (err) {
-      console.log(err);
+      toast.err(`Something is Wrong ${err} !`);
     }
-    console.log(currentRole);
+  };
+
+  // Admin delete a users functions
+  const handleDeleted = async (id) => {
+    try {
+      const { data } = await axios.delete(`/users/${id}`);
+      toast.success("User deleted Successfully");
+    } catch (err) {
+      toast.err(`Something is Wrong ${err}`);
+    }
   };
 
   return (
@@ -41,7 +51,10 @@ const RoleModal = ({ isOpen, setIsOpen, item, refetch }) => {
           >
             Admin
           </button>
-          <button className="bg-gradient-to-r hover:from-primary hover:to-secondary bg-gray-600 py-2 px-7 font-medium text-white rounded-full">
+          <button
+            onClick={() => handleDeleted(item?._id)}
+            className="bg-gradient-to-r hover:from-primary hover:to-secondary bg-gray-600 py-2 px-7 font-medium text-white rounded-full"
+          >
             Remove
           </button>
         </div>
