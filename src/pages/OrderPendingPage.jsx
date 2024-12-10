7ca45ec3a10f0276d42bf1f2ca52773c59271e8c
mdimranhossainwd <mdimranhossain7498@gmail.com>
2024-12-10 13:31:36 +0600

    data: get pending all data

--- src/pages/OrderPendingPage.jsx
+++ src/pages/OrderPendingPage.jsx
@@ -1,6 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxios from "../hooks/useAxios";
 import Heading from "../shared/Heading";
 
 const OrderPendingPage = () => {
+  const axios = useAxios();
+
+  const getData = async () => {
+    const { data } = await axios.get("/pending-product");
+    return data;
+  };
+
+  const { data: getPendingInfo, refetch } = useQuery({
+    queryKey: "getPendingInfo",
+    queryFn: getData,
+  });
+
+  console.log(getPendingInfo);
+
   return (
     <>
       <Heading heading="Pending Orders List" />
