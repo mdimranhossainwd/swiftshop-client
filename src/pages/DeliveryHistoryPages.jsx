73ee4d4ce73c985ee215b84663e77ccbad13582e
mdimranhossainwd <mdimranhossain7498@gmail.com>
2025-01-04 13:16:20 +0600

    fetch: fetch to orderStatus Delivered data

--- src/pages/DeliveryHistoryPages.jsx
+++ src/pages/DeliveryHistoryPages.jsx
@@ -1,6 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxios from "../hooks/useAxios";
 import Heading from "../shared/Heading";
 
 const DeliveryHistoryPages = () => {
+  const axios = useAxios();
+
+  const getData = async () => {
+    const { data } = await axios.get("/delivered-product");
+    return data;
+  };
+
+  const { data: deliveryProducts, refetch } = useQuery({
+    queryKey: ["deliveryProducts"],
+    queryFn: getData,
+  });
+
+  console.log(deliveryProducts);
+
   return (
     <>
       <Heading heading="Completed All Delivery Products" />
