5c608e2b9bd1add769a98ea6951cebc445632733
mdimranhossainwd <mdimranhossain7498@gmail.com>
2025-01-07 14:02:44 +0600

    ui: some sections improved

--- src/components/blogs/BlogLeftSide.jsx
+++ src/components/blogs/BlogLeftSide.jsx
@@ -4,31 +4,33 @@ const BlogLeftSide = () => {
       <aside className="w-full lg:w-1/4 border p-6 rounded-lg mb-6 lg:mb-0">
         {/* Search Blog */}
         <div className="mb-8">
-          <h2 className="text-lg font-semibold mb-2">Search Blog</h2>
+          <h2 className="text-lg font-semibold mb-2 border-b py-2">
+            Search Blog
+          </h2>
           <input
             type="text"
             placeholder="Search blog"
-            className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+            className="w-full mt-3 p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
           />
         </div>
 
         {/* Social Links */}
         <div className="mb-8">
-          <h2 className="text-lg font-semibold mb-2">Social</h2>
+          <h2 className="text-lg font-semibold mb-2 border-b py-2">Social</h2>
           <div className="flex gap-3">
-            <button className="bg-blue-500 text-white p-2 rounded-full">
+            <button className="bg-blue-500 text-white px-3 py-1 rounded-full">
               T
             </button>
-            <button className="bg-red-500 text-white p-2 rounded-full">
+            <button className="bg-red-500 text-white px-3 py-1 rounded-full">
               P
             </button>
-            <button className="bg-pink-500 text-white p-2 rounded-full">
+            <button className="bg-pink-500 text-white px-3 py-1 rounded-full">
               I
             </button>
-            <button className="bg-blue-400 text-white p-2 rounded-full">
+            <button className="bg-blue-400 text-white px-3 py-1 rounded-full">
               F
             </button>
-            <button className="bg-yellow-400 text-white p-2 rounded-full">
+            <button className="bg-yellow-400 text-white px-3 py-1 rounded-full">
               S
             </button>
           </div>
@@ -36,7 +38,9 @@ const BlogLeftSide = () => {
 
         {/* Categories */}
         <div>
-          <h2 className="text-lg font-semibold mb-2">Categories</h2>
+          <h2 className="text-lg font-semibold mb-2  border-b py-2">
+            Categories
+          </h2>
           <ul className="space-y-2">
             <li className="flex justify-between">
               <span>Technology</span> <span>(06)</span>
--- src/pages/BlogsPages.jsx
+++ src/pages/BlogsPages.jsx
@@ -5,10 +5,13 @@ import Heading from "../components/shared/Heading";
 const BlogsPages = () => {
   return (
     <>
-      <Heading heading="All Blogs Here" />
-      <div className="flex flex-wrap lg:flex-nowrap container mx-auto px-4 gap-6 py-8">
-        <BlogLeftSide />
-        <BlogRightSide />
+      <div className="my-8">
+        <Heading heading="All Blogs Here" />
+        <br />
+        <div className="flex flex-wrap lg:flex-nowrap container mx-auto px-4 gap-6 py-8">
+          <BlogLeftSide />
+          <BlogRightSide />
+        </div>
       </div>
     </>
   );
