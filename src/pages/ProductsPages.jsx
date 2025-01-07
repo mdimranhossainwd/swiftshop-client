327a211e8e24e1624da5026ff0d7299b0f679487
mdimranhossainwd <mdimranhossain7498@gmail.com>
2025-01-07 13:32:02 +0600

    ui: ProductPages Heading Added

--- src/pages/ProductsPages.jsx
+++ src/pages/ProductsPages.jsx
@@ -1,5 +1,11 @@
+import Heading from "../shared/Heading";
+
 const ProductsPages = () => {
-  return <></>;
+  return (
+    <>
+      <Heading heading="All Products Here" />
+    </>
+  );
 };
 
 export default ProductsPages;
