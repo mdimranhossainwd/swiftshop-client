015829b7706281872ff4e3d4dd0d21791eb20a26
mdimranhossainwd <mdimranhossain7498@gmail.com>
2025-01-21 13:38:38 +0600

    ui: revenue card border rounded fixed

--- src/pages/RevenuePages.jsx
+++ src/pages/RevenuePages.jsx
@@ -5,22 +5,22 @@ const RevenuePages = () => {
     <>
       <Heading heading="Revenue Analytics " />
       <div className="container mx-auto p-5">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 ">
-          <div className="bg-blue-500 p-10 text-white">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3">
+          <div className="bg-blue-500 rounded-lg p-10 text-white">
             <span className="font-bold text-3xl">1000</span>
             <h3 className="text-xl font-semibold ">Total Revenue</h3>
           </div>
-          <div className="bg-green-500 p-10 text-white">
+          <div className="bg-green-500 rounded-lg p-10 text-white">
             <span className="font-bold text-3xl">1000</span>
 
             <h3 className="text-xl font-semibold ">Total Client</h3>
           </div>
-          <div className="bg-orange-500 p-10 text-white">
+          <div className="bg-orange-500 rounded-lg p-10 text-white">
             <span className="font-bold text-3xl">1000</span>
 
             <h3 className="text-xl font-semibold ">Total Order</h3>
           </div>
-          <div className="bg-purple-500 p-10 text-white">
+          <div className="bg-purple-500 rounded-lg p-10 text-white">
             <span className="font-bold text-3xl">1000</span>
 
             <h3 className="text-xl font-semibold ">Total Customer</h3>
