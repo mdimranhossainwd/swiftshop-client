c5637b83856f369b7aeba3df9d0d31a6d630b641
mdimranhossainwd <mdimranhossain7498@gmail.com>
2025-01-21 13:35:18 +0600

    ui: Revenue card added

--- src/pages/RevenuePages.jsx
+++ src/pages/RevenuePages.jsx
@@ -4,6 +4,29 @@ const RevenuePages = () => {
   return (
     <>
       <Heading heading="Revenue Analytics " />
+      <div className="container mx-auto p-5">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 ">
+          <div className="bg-blue-500 p-10 text-white">
+            <span className="font-bold text-3xl">1000</span>
+            <h3 className="text-xl font-semibold ">Total Revenue</h3>
+          </div>
+          <div className="bg-green-500 p-10 text-white">
+            <span className="font-bold text-3xl">1000</span>
+
+            <h3 className="text-xl font-semibold ">Total Client</h3>
+          </div>
+          <div className="bg-orange-500 p-10 text-white">
+            <span className="font-bold text-3xl">1000</span>
+
+            <h3 className="text-xl font-semibold ">Total Order</h3>
+          </div>
+          <div className="bg-purple-500 p-10 text-white">
+            <span className="font-bold text-3xl">1000</span>
+
+            <h3 className="text-xl font-semibold ">Total Customer</h3>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
