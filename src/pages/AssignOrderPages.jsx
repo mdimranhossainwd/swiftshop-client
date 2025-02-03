b169198d84513314f4f957d4e06cacf24dfb18d0
mdimranhossainwd <mdimranhossain7498@gmail.com>
2025-02-03 18:58:30 +0600

    ui: paginations added

--- src/pages/AssignOrderPages.jsx
+++ src/pages/AssignOrderPages.jsx
@@ -171,6 +171,43 @@ const AssignOrderPages = () => {
             </tbody>
           </table>
         </div>
+
+        <div className="flex items-center justify-center mt-10 mb-5">
+          <a
+            href="#"
+            className="flex items-center px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform  rounded-md dark:bg-gray-800 font-semibold bg-gray-300 hover:bg-blue-600 dark:hover:bg-blue-500 hover:text-white "
+          >
+            previous
+          </a>
+
+          <a
+            href="#"
+            className="items-center hidden px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform  rounded-md sm:flex dark:bg-gray-800 dark:text-gray-200 hover:bg-blue-600 dark:hover:bg-blue-500 dark:hover:text-gray-200"
+          >
+            1
+          </a>
+
+          <a
+            href="#"
+            className="items-center hidden px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform  rounded-md sm:flex dark:bg-gray-800 dark:text-gray-200 hover:bg-blue-600 dark:hover:bg-blue-500 dark:hover:text-gray-200"
+          >
+            2
+          </a>
+
+          <a
+            href="#"
+            className="items-center hidden px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform  rounded-md sm:flex dark:bg-gray-800 dark:text-gray-200 hover:bg-blue-600 dark:hover:bg-blue-500 dark:hover:text-gray-200"
+          >
+            3
+          </a>
+
+          <a
+            href="#"
+            className="flex items-center px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform  rounded-md dark:bg-gray-800 font-semibold bg-gray-300 hover:bg-blue-600 dark:hover:bg-blue-500 hover:text-white "
+          >
+            Next
+          </a>
+        </div>
       </div>
     </>
   );
