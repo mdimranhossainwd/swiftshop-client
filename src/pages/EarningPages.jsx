79014f2717ac1087a7ded7737ca7209917715547
mdimranhossainwd <mdimranhossain7498@gmail.com>
2025-02-12 14:48:16 +0600

    chart: Earnings Charts Added

--- src/pages/EarningPages.jsx
+++ src/pages/EarningPages.jsx
@@ -1,9 +1,33 @@
+import Chart from "react-google-charts";
 import Heading from "../shared/Heading";
 
 const EarningPages = () => {
+  const data = [
+    ["Year", "Sales", "Expenses"],
+    ["2013", 1000, 400],
+    ["2014", 1170, 460],
+    ["2015", 660, 1120],
+    ["2016", 1030, 540],
+  ];
+
+  const options = {
+    title: "Earnings Performance",
+    curveType: "function",
+    legend: { position: "bottom" },
+  };
+
   return (
     <>
       <Heading heading="Earning Overview" />
+      <div className="px-6 mt-8">
+        <Chart
+          chartType="LineChart"
+          width="100%"
+          height="400px"
+          data={data}
+          options={options}
+        />
+      </div>
     </>
   );
 };
